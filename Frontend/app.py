695a86be4c26e726ff76336c15f46024be4fb8a3
felijoao08 <felipe.joao1@escolar.ifrn.edu.br>
2023-12-27 09:23:43 -0300

    continuando o metodo cadastrar

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,16 +1,22 @@
 from flask import Flask,  request, jsonify, render_template, redirect, flash, session
-from blushGlamour import BancoDeDados, Pessoa, Cliente
+from blushGlamour import *
+from datetime import datetime
+
 import mysql.connector
 
  
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "568425806"
 
+#conexao com o banco de dados
+bd = BancoDeDados()
+db = bd.linkarBancoDeDados()
+
 #PAGINA PRINCIPAL DO SITE
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def blushGlamour():
    return render_template('paginaPrincipal.html')
-@app.route('/BlushGlamour', methods=['POST'])
+@app.route('/BlushGlamour', methods=['GET', 'POST'])
 def homepage_blushGlamour():
     return redirect('/BlushGlamour')
 
@@ -18,10 +24,10 @@ def homepage_blushGlamour():
 
 
 #PAGINA DE LOGIN DO CLIENTE
-@app.route('/BlushGlamour-login')
+@app.route('/BlushGlamour-login', methods=['GET', 'POST'])
 def LoginCliente():
    return render_template('loginCliente.html')
-@app.route('/BlushGlamour-login', methods=['POST'])
+@app.route('/BlushGlamour-login', methods=['GET', 'POST'])
 def login_Cliente():
     return redirect('/BlushGlamour-login')
 
@@ -29,10 +35,10 @@ def login_Cliente():
 
 
 #PAGINA DE CADASTRO DO CLIENTE
-@app.route('/BlushGlamour-cadastro')
+@app.route('/BlushGlamour-cadastro', methods=['GET', 'POST'])
 def cadastroCliente():
    return render_template('cadastroCliente.html')
-@app.route('/BlushGlamour-cadastro', methods=['POST'])
+@app.route('/BlushGlamour-cadastro', methods=['GET', 'POST'])
 def cadastro_Cliente():
      cpf = request.form.get('cpf')
      nome = request.form.get('nome')
@@ -45,14 +51,13 @@ def cadastro_Cliente():
      estado = request.form.get('estado')
      cep= request.form.get('cep')
      bairro = request.form.get('bairro')
+     NumeroResidencia = 456
      complemento = request.form.get('Complemento')
-        
-     #esta classe serve para sempre linkar o banco de dados
-     bd = BancoDeDados()
-     bd = bd.linkarBancoDeDados()
-     cliente = Cliente(cpf, nome, email, senha, telefone, dataNascimento, rua, cidade, estado, cep, bairro, complemento)
-     cliente.cadastrar(bd)
+     imagemPerfil = "imagem.jpg"
 
+     #esta classe serve para sempre linkar o banco de dados
+     cliente = Cliente(cpf, nome, email, senha, telefone, dataNascimento, rua, cidade, cep, estado, NumeroResidencia, complemento, bairro, imagemPerfil)
+     cliente.cadastrar(db)
      return redirect('/')
 
         
@@ -60,10 +65,10 @@ def cadastro_Cliente():
 
 
 #PAGINA DE LOGIN DO FORNECEDOR
-@app.route('/BlushGlamour-Fornecedores')
+@app.route('/BlushGlamour-Fornecedores', methods=['GET', 'POST'])
 def loginFornecedor():
    return render_template('loginFornecedores.html')
-@app.route('/BlushGlamour-Fornecedores', methods=['POST'])
+@app.route('/BlushGlamour-Fornecedores',  methods=['GET', 'POST'])
 def login_Fornecedor():
     return redirect('/BlushGlamour-Fornecedores')
 
@@ -71,10 +76,10 @@ def login_Fornecedor():
 
 
 #PAGINA DE CADASTRO DO FORNECEDOR
-@app.route('/BlushGlamour-CadastroFornecedores')
+@app.route('/BlushGlamour-CadastroFornecedores', methods=['GET', 'POST'])
 def cadastroFornecedor():
    return render_template('cadastroFornecedor.html')
-@app.route('/BlushGlamour-CadastroFornecedores', methods=['POST'])
+@app.route('/BlushGlamour-CadastroFornecedores', methods=['GET', 'POST'])
 def cadastro_Fornecedor():
     return redirect('/BlushGlamour-CadastroFornecedores')
 
@@ -82,10 +87,10 @@ def cadastro_Fornecedor():
 
 
 #PAGINA DE ALTERAR DADOS
-@app.route('/alterarDados')
+@app.route('/alterarDados', methods=['GET', 'POST'])
 def alterarDados():
    return render_template('alterarDadosCliente.html')
-@app.route('/alterarDados', methods=['POST'])
+@app.route('/alterarDados', methods=['GET', 'POST'])
 def alterar_dados():
     return redirect('/alterarDados')
 
@@ -93,10 +98,10 @@ def alterar_dados():
 
 
 #PAGINA DE ESQUECI A SENHA
-@app.route('/esqueciSenha')
+@app.route('/esqueciSenha', methods=['GET', 'POST'])
 def esqueciSenha():
    return render_template('esqueciSenha.html')
-@app.route('/esqueciSenha', methods=['POST'])
+@app.route('/esqueciSenha', methods=['GET', 'POST'])
 def esqueci_Senha():
     return redirect('/esqueciSenha')
 
@@ -104,10 +109,10 @@ def esqueci_Senha():
 
 
 #PAGINA DO CARRINHO
-@app.route('/BlushGlamour-carrinho')
+@app.route('/BlushGlamour-carrinho', methods=['GET', 'POST'])
 def carrinho():
    return render_template('carrinho.html')
-@app.route('/BlushGlamour-carrinho', methods=['POST'])
+@app.route('/BlushGlamour-carrinho', methods=['GET', 'POST'])
 def carrinho_compra():
     return redirect('/BlushGlamour-carrinho')
 
@@ -115,10 +120,10 @@ def carrinho_compra():
 
 
 #PAGINA DO PRODUTO
-@app.route('/BlushGlamour-Produto')
+@app.route('/BlushGlamour-Produto', methods=['GET', 'POST'])
 def paginaProduto():
    return render_template('paginaProduto.html')
-@app.route('/BlushGlamour-Produto', methods=['POST'])
+@app.route('/BlushGlamour-Produto',  methods=['GET', 'POST'])
 def pagina_produto():
     return redirect('/BlushGlamour-Produto')
 
@@ -126,10 +131,10 @@ def pagina_produto():
 
 
 #PAGINA DA BARRA DE PESQUISA
-@app.route('/pesquisa')
+@app.route('/pesquisa', methods=['GET', 'POST'])
 def barraPesquisa():
    return render_template('barradePesquisa.html')
-@app.route('/pesquisa', methods=['POST'])
+@app.route('/pesquisa', methods=['GET', 'POST'])
 def barra_Pesquisa():
     return redirect('/pesquisa')
 
--- Frontend/blushGlamour.py
+++ Frontend/blushGlamour.py
@@ -1,15 +1,14 @@
 from flask import Flask,  request, jsonify, render_template, redirect, flash, session
 import mysql.connector
 
-
 class BancoDeDados():
     def __init__(self):
         self.host = 'localhost'
         self.user = 'root'
-        self.password = 'felipe09'
+        self.password = 'labinfo'
         self.database = 'mydb'
-    
-    #esse metodo deve ser chamado toda vez que for usar o BD
+        
+    # Este método deve ser chamado toda vez que for usar o BD
     def linkarBancoDeDados(self):
         try:
             db = mysql.connector.connect(
@@ -18,14 +17,15 @@ class BancoDeDados():
                 password = self.password,
                 database = self.database
             )
-        except mysql.connector.Error as err:
-            print(f"Erro ao conectar ao banco de dados: {err}")
+            return db
+        #se caso ouver erro de conexão com o BD
+        except mysql.connector.Error as e:
+            print(f'Erro: Conexão com o banco de dados - {e}')
             return None
-        return db
 
 
 class Pessoa:
-    def __init__(self, cpf, nome, email, senha, telefone, dataNascimento, rua=None,cidade=None, cep=None, estado=None, NumeroResidencia=None, Complemento=None, bairro=None):
+    def __init__(self, cpf, nome, email, senha, telefone, dataNascimento, rua=None,cidade=None, cep=None, estado=None, NumeroResidencia=None, Complemento=None, bairro=None, imagemPerfil=None):
         self.cpf = cpf
         self.nome = nome
         self.email = email
@@ -39,14 +39,21 @@ class Pessoa:
         self.NumeroResidencia = NumeroResidencia
         self.Complemento = Complemento
         self.bairro = bairro
+        self.imagemPerfi = imagemPerfil
         
     def cadastrar(self, db):
-        cursor = db.cursor(dictionary=True)
-        cursor.execute("INSERT INTO Cliente (cpf, nome, email, senha, dataNascimento, imagemPerfil) VALUES (%s, %s, %s, %s, %s, %s)", (self.cpf, self.nome, self.email, self.senha, self.dataNascimento))
-        cursor.execute("INSERT INTO EnderecosCliente (rua, cidade, cep, estado, numResidencia, Complemento, Cliente_cpf, bairro) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)" , (self.rua, self.cidade, self.cep, self.estado, self.NumeroResidencia, self.Complemento, self.bairro))
-        db.commit()
-        cursor.close()
-
+        try:
+            cursor = db.cursor(dictionary=True)
+            cursor.execute("INSERT INTO Cliente (cpf, nome, email, senha, dataNascimento, imagemPerfil) VALUES (%s, %s, %s, %s, %s, %s)", (self.cpf, self.nome, self.email, self.senha, self.dataNascimento, self.imagemPerfi))
+            cursor.execute("INSERT INTO EnderecosCliente (rua, cidade, cep, estado, numResidencia, Complemento, Cliente_cpf, bairro) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (self.rua, self.cidade, self.cep, self.estado, self.NumeroResidencia, self.Complemento, self.cpf, self.bairro))
+            db.commit()
+            cursor.close()
+        #se caso der erro nas inserções 
+        except mysql.connector.Error as e:
+            print(f"Erro ao inserir dados no banco de dados: {e}")
+        #se caso der erro quando tentat duplicar dados UNIQUE 
+        except mysql.connector.Error as e:
+            print(f"Erro ao inserir dados no banco de dados: {e}")
 
     def login(self):
         pass
@@ -62,8 +69,8 @@ class Pessoa:
 
 
 class Cliente(Pessoa):
-    def __init__(self, cpf, nome, email, senha, telefone, dataNascimento, rua=None, bairro=None, cidade=None, cep=None, estado=None, NumeroResidencia=None, Complemento=None):
-        super().__init__(cpf, nome, email, senha, telefone, dataNascimento, rua, cidade, cep, estado, NumeroResidencia, Complemento, bairro)
+    def __init__(self, cpf, nome, email, senha, telefone, dataNascimento, rua=None, cidade=None, cep=None, estado=None, NumeroResidencia=None, Complemento=None, bairro=None, imagemPerfil=None):
+        super().__init__(cpf, nome, email, senha, telefone, dataNascimento, rua, cidade, cep, estado, NumeroResidencia, Complemento, bairro, imagemPerfil)
 
 class Fornecedor(Pessoa):
     def __init__(self, cnpj, nome, email, senha, telefone, dataNascimento, rua=None, bairro=None, cidade=None, cep=None, estado=None, NumeroResidencia=None, Complemento=None):
@@ -107,3 +114,33 @@ class Carrinho():
     
     def FinalizarCompra(self):
         pass
+
+
+
+
+#!!!!!!!!!!!AEA PARA TESTES!!!!!!!!!!
+
+
+#TESTE DAS INSERÇÔES DO CADASTRO
+bd = BancoDeDados()
+db = bd.linkarBancoDeDados()
+print(db)
+
+
+cpf = '21499868'
+nome = 'joao'
+email = '@vanna.com'
+senha = '4568'
+telefone = '79979777'
+dataNascimento = '2023-03-15'
+rua = 'rua das ruas'
+cidade = 'ceara mirim'
+estado = 'RN'
+cep= '59570000'
+bairro = 'planalto'
+NumeroResidencia = '456'
+complemento = 'perto da farmacia'
+imagemPerfil = "imagem.jpg"
+
+cliente = Cliente(cpf, nome, email, senha, telefone, dataNascimento, rua, cidade, cep, estado, NumeroResidencia, complemento, bairro, imagemPerfil)
+cliente.cadastrar(db)
